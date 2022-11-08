cc7819e6fbb92d7c73043f490c03ff350cc989a6
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-11-07 22:57:47 -0500

    remove testing

--- .github/workflows/submission_entrypoints.py
+++ .github/workflows/submission_entrypoints.py
@@ -1,33 +1,7 @@
 import ast
 import sys
 
-###
-
-import os
-import subprocess
-from typing import Dict, Union, List
-
-# from brainscore_core.submission.endpoints import process_github_submission
-
-JENKINS_BASE = "braintree.mit.edu:8080"
-USER = os.environ['JENKINS_USR']
-JENKINS_TOKEN = os.environ['JENKINS_TOKEN']
-JOB_PATH = "dev_run_benchmarks"
-
-def process_github_submission(plugin_info: Dict[str, Union[List[str], str]]):
-	"""
-	Triggered when changes are merged to the GitHub repository, if those changes affect benchmarks or models.
-	Starts parallel runs to score models on benchmarks (`run_scoring`).
-	"""
-	model_list = plugin_info['models'].split()
-	benchmark_list = plugin_info['benchmarks'].split()
-
-	models = model_list if len(model_list) > 0 else ['all']
-	benchmarks = benchmark_list if len(benchmark_list) > 0 else ['all']
-	for model in models:
-		for benchmark in benchmarks:
-			url = f'{JENKINS_BASE}/job/{JOB_PATH}/buildWithParameters?models={model}benchmarks={benchmark}&token={JENKINS_TOKEN}'
-			response = subprocess.run(f"curl -X POST -u {USER}:{JENKINS_TOKEN} {url}", shell=True)
+from brainscore_core.submission.endpoints import process_github_submission
 
 
 if __name__ == '__main__':
