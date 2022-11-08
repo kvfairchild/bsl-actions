2e407136738b57d57c74a32b67f79269939846fe
Katherine Fairchild <kvfairchild@Katherines-MIT-Mac.local>
2022-11-08 12:16:08 -0500

    debug

--- .github/workflows/submission_entrypoints.py
+++ .github/workflows/submission_entrypoints.py
@@ -7,4 +7,5 @@ from brainscore_core.submission.endpoints import process_github_submission
 if __name__ == '__main__':
 	function = getattr(sys.modules[__name__], sys.argv[1])
 	args_dict = ast.literal_eval(sys.argv[2])
-	function(args_dict)
+	calls = function(args_dict)
+	print(calls)
